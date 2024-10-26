d1fbfbbf9cdd4af0070b46e4deff69dc55c6be7d
Anushka Sah <sahanushka1820@gmail.com>
2024-10-26 18:12:05 +0530

    added a new repository

---
+++ src/client/src/components/Card.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Modal from './Modal';
+import { useSupplier } from '../context/supplierContext';
+
+const Card = ({ cardData, deleteEvent }) => {
+    const navigate = useNavigate();
+    const { setCurrentDoc } = useSupplier();
+    return (
+        <>
+            <div className="card bg-secondary text-light" style={{ width: '18rem' }}>
+                <div className="card-body">
+                    <h5 className="card-title">{cardData?.title}</h5>
+                    <h6 >Owner: {cardData?.owner?.username} || {cardData?.createdAt?.split('T')[0]}</h6>
+                    <p className="card-text">
+                        {cardData?.content?.ops[0]?.insert?.slice(0, 50) || ''}
+                        {cardData?.content?.ops[0]?.insert?.length > 50 ? '...' : ''}
+                    </p>
+                    <button className="btn btn-danger me-4" data-bs-toggle="modal" data-bs-target={`#${"deleteDoc" + cardData?._id}`}>Delete</button>
+                    <button href="#" onClick={() => { navigate(`/edit/${cardData._id}`); setCurrentDoc(cardData) }} className="btn btn-success">Edit</button>
+                </div>
+            </div>
+
+            <Modal title={"Delete Document"} modalId={"deleteDoc" + cardData?._id} content={
+                <>
+                    <div className='col-md-12'>
+                        <p className='lead display-6'>Are you sure you want to delete {cardData?.title} document?</p>
+                    </div>
+                    <div className="mb-3 d-flex justify-content-end">
+
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" onClick={() => deleteEvent(cardData._id)} data-bs-dismiss="modal" className="btn btn-danger ms-2">Delete</button>
+                    </div>
+                </>
+
+            } />
+
+        </>
+    )
+}
+
+export default Card
---
+++ src/client/src/pages/main/EditDocument.jsx
@@ -0,0 +1,289 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSupplier } from '../../context/supplierContext';
+import { toast } from 'react-toastify';
+import Modal from '../../components/Modal';
+import { addCollaboratorToDoc, getAllCollaborators } from '../../helpers/docs/doc.helper';
+import { useAuth } from '../../context/authContext';
+import { API } from '../../helpers/config';
+import Editor from './Editor.jsx';
+
+const EditDocument = () => {
+    const [currentUsers, setCurrentUsers] = useState([]);
+    const [collaboratorEmail, setCollaboratorEmail] = useState('');
+    const [collaborators, setCollaborators] = useState([]);
+    const [isModified, setIsModified] = useState(false);  // State to track document modifications
+
+    const navigate = useNavigate();
+    const { auth } = useAuth();
+    const { currentDoc, loading, socket, setCurrentDoc, darkMode, triggerUpdate, quill, setLoading } = useSupplier();
+    const { id } = useParams();
+
+    const handleAddCollaborator = async () => {
+        setLoading(true);
+        const res = await addCollaboratorToDoc(currentDoc?._id, collaboratorEmail, auth?.token).finally(() => setLoading(false));
+        if (res?.status === 200) {
+            setCollaboratorEmail('');
+            document.getElementById('closeTheModal').click();
+            toast.success(res?.data?.message);
+            triggerUpdate();
+            return;
+        }
+        toast.error(res?.data?.message);
+    };
+
+    useEffect(() => {
+        if (quill == null || !currentDoc?._id) return;
+
+        socket.emit("get-doc", { docId: currentDoc?._id });
+
+        socket.once('load-document', document => {
+            quill.setContents(document);
+            quill.enable();
+        });
+    }, [quill, socket, currentDoc]);
+
+    // Register document modifications and mark as modified
+    useEffect(() => {
+        if (quill == null || !currentDoc?._id) return;
+
+        const handleTextChange = (delta, oldDelta, source) => {
+            if (source === 'user') {
+                setIsModified(true);  // Mark as modified when the user makes changes
+            }
+        };
+
+        quill.on('text-change', handleTextChange);
+
+        return () => {
+            quill.off('text-change', handleTextChange);
+        };
+    }, [quill, currentDoc]);
+
+    // Check and save only if there are modifications
+    useEffect(() => {
+        if (quill == null) return;
+
+        const interval = setInterval(() => {
+            if (isModified) {
+                toast.info('Saving document...');
+                socket.emit("save-doc", { docId: currentDoc?._id, data: quill?.getContents() }, (error) => {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        toast.success('Document saved successfully');
+                        setIsModified(false);  // After saving, mark as not modified
+                    }
+                });
+            }
+        }, 30000);  // Interval to check for modifications
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [quill, isModified, currentDoc]);
+
+    // Function to save immediately when necessary (can be called when leaving the page, for example)
+    const saveDocumentImmediately = () => {
+        if (isModified) {
+            socket.emit("save-doc", { docId: currentDoc?._id, data: quill?.getContents() }, (error) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    toast.success('Document saved successfully');
+                    setIsModified(false);  // After saving, mark as not modified
+                }
+            });
+        }
+    };
+
+    // Other collaborator and room functionalities
+    useEffect(() => {
+        const fetchCollaborators = async () => {
+            setLoading(true);
+            const res = await getAllCollaborators(currentDoc?._id, auth?.token).finally(() => setLoading(false));
+            if (res?.status === 200) {
+                setCollaborators(res?.data?.collaborators);
+            }
+        };
+
+        const resetCurrentDocStateOnReload = async () => {
+            const fetchDoc = await fetch(`${API}/documents/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${auth?.token}`
+                }
+            });
+            const doc = await fetchDoc.json();
+            setCurrentDoc(doc?.document);
+        };
+
+        if (!currentDoc && auth?.token) {
+            resetCurrentDocStateOnReload();
+        }
+        if (auth?.token && currentDoc?._id) {
+            fetchCollaborators();
+        }
+    }, [auth, currentDoc, setCurrentDoc]);
+
+    useEffect(() => {
+        if (!quill || !currentDoc?._id) return;
+    
+        const handler = (delta, oldDelta, source) => {
+            if (source !== 'user') return;
+            
+            socket.emit('send-changes', { delta, roomId: currentDoc?._id, username: auth?.user?.username });
+        };
+    
+        quill.on('text-change', handler);
+    
+        return () => {
+            if (quill) {
+                quill.off('text-change', handler);  // Remove event listener using reference
+            }
+        };
+    }, [quill, socket, currentDoc, auth?.user?.username]);
+    
+
+    useEffect(() => {
+        if (quill == null || !currentDoc?._id) return;
+
+        const handleSomeoneJoined = (data) => {
+            setCurrentUsers(data?.roomUsers);
+        };
+
+        const handleSomeoneLeft = (data) => {
+            setCurrentUsers(data?.roomUsers);
+        };
+
+        socket.emit('joinRoom', { roomId: currentDoc?._id, username: auth?.user?.username }, (error) => {
+            if (error) {
+                console.error('Error joining room:', error);
+            }
+        });
+
+        socket.on('someoneJoined', handleSomeoneJoined);
+        socket.on('someoneLeft', handleSomeoneLeft);
+
+        return () => {
+            if (quill) {
+                quill.disable();
+            }
+
+            socket.emit('leaveRoom', { roomId: currentDoc?._id, username: auth?.user?.username }, (error) => {
+                if (error) {
+                    console.error('Error leaving room:', error);
+                }
+            });
+
+            socket.off('someoneJoined', handleSomeoneJoined);
+            socket.off('someoneLeft', handleSomeoneLeft);
+            setCurrentUsers([]);
+        };
+    }, [currentDoc, socket, auth?.user?.username]);
+
+    useEffect(() => {
+        socket.on('receive-changes', (data) => {
+            if(data?.username === auth?.user?.username) return;
+            quill.updateContents(data?.delta);
+        });
+
+        return () => {
+            socket.off('receive-changes');
+        };
+    }, [currentDoc, socket]);
+
+    return (
+        <div className={`container my-4 ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+            <div className='row d-flex justify-content-center align-items-center'>
+                <div className='my-3 col-lg-10 col-xl-8 d-flex justify-content-between align-items-center'>
+                    <div className='d-flex align-items-center'>
+                        <button type="button" className={`btn btn-warning me-2 ${darkMode ? 'text-light' : ''}`} onClick={() =>{
+                            saveDocumentImmediately();
+                            navigate('/home');
+                        }}>
+                            <i className="bi bi-arrow-left"></i>
+                        </button>
+                    </div>
+                    <div className='d-flex align-items-center'>
+                        <h1 className={`display-6 text-center ${darkMode ? 'text-light' : 'text-dark'}`}>
+                            Document Title: <u> {currentDoc?.title}</u>
+                            <button
+                                type="button"
+                                className='btn btn-secondary ms-5'
+                                data-bs-toggle="modal"
+                                data-bs-target={`#${"collaborators"}`}
+                            >
+                                <i className="bi bi-eye-fill"></i>
+                            </button>
+                        </h1>
+                    </div>
+                    <div className='d-flex align-items-center'>
+                        <button
+                            type="button"
+                            className="btn btn-primary me-2"
+                            data-bs-toggle="modal"
+                            data-bs-target={`#${"addCollaborator"}`}
+                        >
+                            <i className="bi bi-person-fill-add"></i>&nbsp;Add Collaborator
+                        </button>
+                    </div>
+                </div>
+                <div className='col-md-12 col-lg-2 col-xl-4 mb-4'>
+                    <ul className={`list-group ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                        <li className={`list-group-item ${darkMode ? 'bg-secondary text-light' : 'bg-secondary text-dark'}`} aria-current="true">
+                            <i className="bi bi-people-fill"></i> Online Collaborators ({currentUsers?.length})
+                        </li>
+                        {currentUsers?.map((user, index) => (
+                            <li key={index} className={`list-group-item ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                                <i className="bi bi-person"></i>&nbsp;{user?.username} {user?.username === auth?.user?.username && '(You)'}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='col-md-12 col-lg-10 col-xl-8'>
+                    <Editor />
+                </div>
+            </div>
+
+            {/* Add Collaborator Modal */}
+            <Modal title={"Add Collaborator"} modalId={"addCollaborator"} content={
+                <>
+                    <div className='col-md-12'>
+                        <p className={`lead text-xl ${darkMode ? 'text-light' : 'text-dark'}`}>Enter the email of the user you want to add as a collaborator</p>
+                    </div>
+                    <div className="mb-3 d-flex justify-content-end">
+                        <input
+                            type="text"
+                            value={collaboratorEmail}
+                            onChange={(e) => setCollaboratorEmail(e.target.value)}
+                            className={`form-control ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}
+                            placeholder={"Email"}
+                        />
+                        <button type="button" className={`btn btn-secondary ms-2 ${darkMode ? 'text-light' : ''}`} data-bs-dismiss="modal">Close</button>
+                        <button onClick={handleAddCollaborator} type="button" className="btn btn-primary ms-2">Add Collaborator</button>
+                    </div>
+                </>
+            } />
+
+            {/* Collaborators Modal */}
+            <Modal title={"Collaborators"} modalId={"collaborators"} content={
+                <>
+                    <ul className={`list-group ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                        <li className={`list-group-item ${darkMode ? 'bg-secondary text-light' : 'bg-primary text-dark'}`} aria-current="true">
+                            <i className="bi bi-person-check-fill"></i>&nbsp;Collaborators {collaborators?.length}
+                        </li>
+                        {collaborators?.map((user, index) => (
+                            <li key={index} className={`list-group-item d-flex justify-content-between ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                                <span><i className="bi bi-person"></i>&nbsp;{user?.username}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            } />
+        </div>
+    );
+};
+
+export default EditDocument;
